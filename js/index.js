996d74c6675426c644ce4072a9c62765c34ac497
Gideon Farrell <me@gideonfarrell.co.uk>
2014-11-15 13:36:45 +0000

    instantiate particles and mailto link on index

--- js/index.js
+++ js/index.js
@@ -1,3 +1,41 @@
-require([], function() {
-    console.log('Hello World');
+require(['jquery', 'particles'], function($, particles) {
+    particles('main', {
+        particles: {
+            color: '#fff',
+            shape: 'circle',
+            opacity: 0.5,
+            size: 2,
+            size_random: true,
+            nb: 75,
+            line_linked: {
+                enable_auto: true,
+                distance: 250,
+                color: '#fff',
+                opacity: 0.5,
+                width: 1,
+                condensed_mode: {
+                    enable: true,
+                    rotateX: 600,
+                    rotateY: 600
+                }
+            },
+            anim: {
+                enable: true,
+                speed: 1
+            }
+        },
+        interactivity: {
+            enable: true,
+            mouse: {
+                distance: 250
+            },
+            detect_on: 'window', // "canvas" or "window"
+            mode: 'grab'
+        },
+        /* Retina Display Support */
+        retina_detect: true
+    });
+
+    var parts = ['me', 'gideonfarrell', 'co.uk'];
+    $('.mailto').attr('href', 'mailto://' + parts[0] + '@' + parts[1] + '.' + parts[2]);
 });
